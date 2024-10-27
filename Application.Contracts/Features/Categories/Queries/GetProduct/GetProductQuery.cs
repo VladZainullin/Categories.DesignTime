604c365ca5e3087964cd3832e6618c1cf5e7f17c
Vladislav Zainullin <mittoface02@mail.ru>
2024-10-27 18:59:29 +0300

    Удаляет запросы продуктов из контроллеров

--- Application.Contracts/Features/Categories/Queries/GetProduct/GetProductQuery.cs
+++
@@ -1,5 +0,0 @@
-using MediatR;
-
-namespace Application.Contracts.Features.Categories.Queries.GetProduct;
-
-public sealed record GetProductQuery(GetProductRequestRouteDto RouteDto) : IRequest<GetProductResponseDto>;
--- Application.Contracts/Features/Categories/Queries/GetProduct/GetProductRequestRouteDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Application.Contracts.Features.Categories.Queries.GetProduct;
-
-public sealed class GetProductRequestRouteDto
-{
-    public required Guid CategoryId { get; init; }
-
-    public required Guid ProductId { get; init; }
-}
