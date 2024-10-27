493bd58701c9ac6d8e28b53fd6e320c9fd0725f8
Vladislav Zainullin <mittoface02@mail.ru>
2024-10-27 16:36:50 +0300

    Добавляет сборку Web

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -5,7 +5,7 @@ namespace Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddMediatR(static mediatorServiceConfiguration =>
         {
